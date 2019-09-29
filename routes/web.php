f31ec89486b76611a3d5dbc57ff145ca7a90b384
Norbert Luksa <norbert.luksa@cloudera.com>
2019-09-29 22:36:58 +0200

    Added authentication

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,10 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
