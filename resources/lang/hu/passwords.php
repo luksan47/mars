aebafd6e36c47ef620fef53c0cb2b99396b1e0c7
katkodominik <56202545+katkodominik@users.noreply.github.com>
2019-10-05 23:05:50 +0200

    Add files via upload

--- resources/lang/hu/passwords.php
+++ resources/lang/hu/passwords.php
@@ -14,7 +14,7 @@ return [
     */
 
     'reset' => 'A jelszavad vissza lett állítva!',
-    'sent' => 'Elküldük az e-mail címedre a jelszó visszaállítási linket!',
+    'sent' => 'Elküldtük az e-mail címedre a jelszó visszaállítási linket!',
     'token' => 'A jelszó visszaállítási kulcsod érvénytelen.',
     'user' => "Ilyen e-mail címen nem találtunk felhasználói fiókot.",
 
---
+++ resources/lang/hu/print.php
@@ -0,0 +1,16 @@
+<?php
+
+return [
+    "print.available_money" => "Rendelkezésre álló összeg",
+    "print.printing_account" => "Nyomtatószámla"
+    "print.document" => "Dokumentum nyomtatása"
+    "print.modify" => "Módosítás"
+    "print.add_free_pages" => "Ingyenes oldalak hozzáadása"
+    "print.history" => "Előzmények"
+    "print.add" => "Hozzáadás"
+    "print.twosided" => "Kétoldalas nyomtatás"
+    "print.pdf_description" => "Csak .pdf fájl nyomtatható."
+    "print.print" => "Nyomtatás"
+    "print.free" => "Ingyenes oldalak"
+
+]
