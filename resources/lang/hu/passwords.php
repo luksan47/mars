b9b7311caabe8986126e131fa890187fbdc565a7
katkodominik <56202545+katkodominik@users.noreply.github.com>
2019-10-06 18:21:16 +0200

    Issue #41: Add hungarian translation  (#46)
    Fixes #46

---
+++ resources/lang/hu/passwords.php
@@ -0,0 +1,21 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | has failed, such as for an invalid token or invalid new password.
+    |
+    */
+
+    'reset' => 'A jelszavad vissza lett állítva!',
+    'sent' => 'Elküldtük az e-mail címedre a jelszó visszaállítási linket!',
+    'token' => 'A jelszó visszaállítási kulcsod érvénytelen.',
+    'user' => "Ilyen e-mail címen nem találtunk felhasználói fiókot.",
+
+];
--- resources/lang/hu/print.php
+++ resources/lang/hu/print.php
@@ -1,5 +1,25 @@
 <?php
 
 return [
-    'print' => 'Nyomtatás'
-];
+    /*
+    |--------------------------------------------------------------------------
+    | Printing Page Language Lines
+    |--------------------------------------------------------------------------
+    |
+    */
+
+    "available_money" => "Rendelkezésre álló összeg",
+    "printing_account" => "Nyomtatószámla"
+    "document" => "Dokumentum nyomtatása"
+    "modify" => "Módosítás"
+    "add_free_pages" => "Ingyenes oldalak hozzáadása"
+    "history" => "Előzmények"
+    "add" => "Hozzáadás"
+    "twosided" => "Kétoldalas nyomtatás"
+    "pdf_description" => "Csak .pdf fájl nyomtatható."
+    "print" => "Nyomtatás"
+    "free" => "Ingyenes oldalak"
+
+]
+  
+ 
