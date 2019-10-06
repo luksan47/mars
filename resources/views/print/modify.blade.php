d4014d0814ebd45058754932f5109eb6f48b9f4f
Norbert Luksa <norbert.luksa@gmail.com>
2019-10-06 22:01:12 +0200

    Issue #30: Printing module: validate data (#39)
    * Issue #30: Printing module: validate data
    
    Added validation business logic. Reverted from using Request classes.

--- resources/views/print/modify.blade.php
+++ resources/views/print/modify.blade.php
@@ -1,13 +1,22 @@
 @if (Gate::allows('print.modify'))
-<div class="card">
-    <div class="card-header">@lang('print.modify')</div>
-    <div class="card-body">
-        <form method="POST" action="{{ route('print.modify') }}">
-            @csrf
-            <input id="user_id" name="user_id" type="number">
-            <input id="balance" name="balance" type="number">
-            <button type="submit" class="btn btn-primary">@lang('print.add')</button>
-        </form>
+    <div class="card">
+        <div class="card-header">@lang('print.modify')</div>
+        @if ($errors->any())
+            <div class="alert alert-danger">
+                <ul>
+                    @foreach ($errors->all() as $error)
+                        <li>{{ $error }}</li>
+                    @endforeach
+                </ul>
+            </div>
+        @endif
+        <div class="card-body">
+            <form method="POST" action="{{ route('print.modify') }}">
+                @csrf
+                <input id="user_id" name="user_id" type="number">
+                <input id="balance" name="balance" type="number">
+                <button type="submit" class="btn btn-primary">@lang('print.add')</button>
+            </form>
+        </div>
     </div>
-</div>
 @endif
