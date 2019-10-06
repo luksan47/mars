497505d56d665d4271c8c865420979b0bc5fa34e
Norbert Luksa <norbert.luksa@gmail.com>
2019-10-06 19:16:02 +0200

    Update UsersTableSeeder

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -12,7 +12,7 @@ class UsersTableSeeder extends Seeder
     public function run()
     {
         DB::table('users')->insert([
-            'name' => 'Soros Orsolya',
+            'name' => 'Hapák József',
             'email' => 'root@eotvos.elte.hu',
             'password' => bcrypt('asdasdasd'),
             'permission' => 1,
