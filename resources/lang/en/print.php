c57cfded6f3cf14a8b94bd79bcdcdbd415c200a1
katkodominik <56202545+katkodominik@users.noreply.github.com>
2019-10-06 18:25:15 +0200

    Create print.php

---
+++ resources/lang/en/print.php
@@ -0,0 +1,25 @@
+<?php
+
+return [
+    /*
+    |--------------------------------------------------------------------------
+    | Printing Page Language Lines
+    |--------------------------------------------------------------------------
+    |
+    */
+
+    "available_money" => "Available money",
+    "printing_account" => "Printing account"
+    "document" => "Print document"
+    "modify" => "Modify"
+    "add_free_pages" => "Add free pages"
+    "history" => "History"
+    "add" => "Add"
+    "twosided" => "Two-sided printing"
+    "pdf_description" => "Only .pdf files can be printed."
+    "print" => "Print"
+    "free" => "Free pages"
+
+]
+  
+ 
