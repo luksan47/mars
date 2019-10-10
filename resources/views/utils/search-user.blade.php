65f456abd484ee03297379b14bc50ad19f4d46a2
katkodominik <56202545+katkodominik@users.noreply.github.com>
2019-10-10 20:14:43 +0200

    Update search-user.blade.php

--- resources/views/utils/search-user.blade.php
+++ resources/views/utils/search-user.blade.php
@@ -1,7 +1,7 @@
 
 <select class="selectpicker" data-live-search="true" name="user-id">
-    <!--{% for x in users %} {% endfor %} -->
-    <option data-tokens="x">x</option>
-    <option data-tokens="y">y</option>
-    <option data-tokens="z">z</option>
+    <!--{% for x in users %} ... {% endfor %} or sth -->
+    <option>x</option>
+    <option>y</option>
+    <option>z</option>
 </select>
