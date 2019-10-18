5266895904c32569a4c6a8b791d2fc634ea05ec0
Norbert Luksa <norbert.luksa@gmail.com>
2019-10-18 23:43:29 +0200

    Issue #90: fix broken scripts

--- resources/views/search-user.blade.php
+++ resources/views/search-user.blade.php
@@ -1,8 +1,5 @@
-
-
 <select class="selectpicker" data-live-search="true" id="user_id" name="user_id" title="@lang('print.user')">
     @foreach ($users as $user)
         <option value="{{ $user->id }}">{{ $user->name }}</option>
-    @endforeach 
-    
+    @endforeach
 </select>
