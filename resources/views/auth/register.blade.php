3351424f1610a121a5da7a84e4212b8a346bbb02
katkodominik <56202545+katkodominik@users.noreply.github.com>
2019-10-27 17:55:19 +0100

    moved register button to the right

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -52,7 +52,7 @@
                         </div>
 
                         <div class="form-group row mb-0">
-                            <div class="col-md-6 offset-md-4">
+                            <div class="col-md-6 offset-mb-4">
                                 <button type="submit" class="btn btn-primary">
                                     {{ __('Register') }}
                                 </button>
