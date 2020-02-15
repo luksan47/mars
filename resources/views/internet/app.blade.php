1d2c9ca8c8bc29218b1e980f96f64eebde2a31f5
Norbert Luksa <norbert.luksa@gmail.com>
2020-02-15 14:28:42 +0100

    readded wifi block

--- resources/views/internet/app.blade.php
+++ resources/views/internet/app.blade.php
@@ -6,11 +6,9 @@
         <div class="col-md-8">
             @include('internet.internet_access.internet_access')
         </div>
-<!--
         <div class="col-md-8">
             @include('internet.wifi_password.wifi_password')
         </div>
--->
         <div class="col-md-8">
             @include('internet.mac_addresses.mac_addresses')
         </div>
