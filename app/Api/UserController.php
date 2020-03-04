2d0ef7a627c9b60ec84a664ee890ac04ae0e55a1
Norbert Luksa <norbert.luksa@gmail.com>
2020-03-04 18:23:09 +0000

    Apply fixes from StyleCI

--- app/Api/UserController.php
+++ app/Api/UserController.php
@@ -3,8 +3,8 @@
 namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
-use App\User;
 use App\Http\Resources\User as UserResource;
+use App\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Validator;
@@ -16,14 +16,14 @@ class UserController extends Controller
     public function login()
     {
         if (Auth::attempt([
-                'email' => request('email'),
-                'password' => request('password')
-            ])) {
+            'email' => request('email'),
+            'password' => request('password'),
+        ])) {
             $user = Auth::user();
-            $success['token'] =  $user->createToken('MyApp')-> accessToken;
-            return response()->json(['success' => $success], $this-> successStatus);
-        }
-        else {
+            $success['token'] = $user->createToken('MyApp')->accessToken;
+
+            return response()->json(['success' => $success], $this->successStatus);
+        } else {
             return response()->json(['error'=>'Unauthorised'], 401);
         }
     }
@@ -41,10 +41,10 @@ class UserController extends Controller
         $input = $request->all();
         $input['password'] = bcrypt($input['password']);
         $user = User::create($input);
-        $success['token'] =  $user->createToken('MyApp')-> accessToken;
-        $success['name'] =  $user->name;
+        $success['token'] = $user->createToken('MyApp')->accessToken;
+        $success['name'] = $user->name;
 
-        return response()->json(['success'=>$success], $this-> successStatus);
+        return response()->json(['success'=>$success], $this->successStatus);
     }
 
     public function details()
