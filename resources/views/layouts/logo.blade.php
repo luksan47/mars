fdc52bab7b6a80736114fb72b45f55ca361682c7
katkodominik <56202545+katkodominik@users.noreply.github.com>
2020-03-14 13:57:47 +0100

    Changed materialize css based on sass

--- resources/views/layouts/logo.blade.php
+++ resources/views/layouts/logo.blade.php
@@ -1,9 +1,8 @@
-<ul class="primary-color" style="padding:20px">
+<ul class="blue-grey darken-1"style="padding:20px">
     <div class="row">
         <div class="col s12 valign-wrapper center-align">
             <!--<img class="responsive-img " style="height:100px" src="img/eotvos_logo.png">-->
-            <!--<i class="material-icons" style="color:white;height:40px">public</i>-->
-            <img class="material-icons" style="height:80px" src="img/mars.png">
+            <img class="material-icons" style="height:80px" src="img/uran_2.png">
             <a class=" white-text" style="text-indent:15px;font-size:40px;text-transform: uppercase;font-weight:300;letter-spacing:3px;"; href="{{ url('/') }}"> {{ config('app.name', 'Urán') }} </a>
         </div>
     </div>
