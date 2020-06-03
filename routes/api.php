89e0014e9fff22db68d26f13fa35c8641921c0d4
Norbert Luksa <norbert.luksa@gmail.com>
2020-06-03 15:14:31 +0000

    Apply fixes from StyleCI

--- routes/api.php
+++ routes/api.php
@@ -14,7 +14,7 @@ use Illuminate\Http\Request;
 */
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
-   return $request->user();
+    return $request->user();
 });
 
 Route::post('login', 'Api\UserController@login');
