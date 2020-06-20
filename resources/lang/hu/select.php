56395c608ad6d3dc29a921f85a25c3c8d7bfecf7
Norbert Luksa <norbert.luksa@cloudera.com>
2020-06-20 21:41:44 +0200

    Modifiable label for selects

---
+++ resources/lang/hu/select.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+
+    'teacher' => 'Oktató',
+    'user_to_send' => 'Kedvezményezett',
+    'user_id_modify' => 'Módosítandó felhasználó',
+    'user_id_free' => 'Módosítandó felhasználó',
+    'user_id' => 'Felhasználó',
+];
