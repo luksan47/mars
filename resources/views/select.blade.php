9cc54b29ef23d2537af38245ac51b764354587bc
Norbert Luksa <norbert.luksa@gmail.com>
2020-06-20 21:01:50 +0200

    Enable select blade for multiple inclusion (#236)

---
+++ resources/views/select.blade.php
@@ -0,0 +1,24 @@
+{{-- In order to use this blade, we need to pass an $elements array with id and name, and UID for the page. --}}
+@php
+  $elements = $elements->sortBy('name')
+@endphp
+<select searchable="@lang('general.search')" id="{{ $element_id }}" name="{{ $element_id }}">
+  @foreach ($elements as $element)
+  <option value="{{ $element->id }}">{{ $element->name }}</option>
+  @endforeach
+</select>
+<label for="{{ $element_id }}">@lang('info.name')</label>
+<script>
+  //Initialize materialize select
+  var instances;
+  $(document).ready(
+    function() {
+      var elems = $('#{{ $element_id }}');
+      const options = [
+        @foreach ($elements as $element)
+        { name : '{{ $element->name }}',  value : '{{ $element->id }}'},
+        @endforeach
+        ]
+      instances = M.FormSelect.init(elems, options);
+});
+</script>
