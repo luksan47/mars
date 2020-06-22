91e9fc3c74e63ff87aa550a359230bb905b911f0
katkodominik <56202545+katkodominik@users.noreply.github.com>
2020-06-22 18:47:17 +0200

    deleted test route for pictures

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -6,7 +6,7 @@
 <img src="https://laravel.com/img/notification-logo.png" class="logo" alt="Laravel Logo">
 @break
 @case('Urán')
-<img src="{{-- https://uran.eotvos.elte.hu/img/uran_blue.png --}}https://mars.local/img/uran_blue.png" width="60" alt="Urán Logó">
+<img src="https://uran.eotvos.elte.hu/img/uran_blue.png" width="60" alt="Urán Logó">
 <span class="logo" > {{ config('app.name') }} </span>
 @break
 @default
