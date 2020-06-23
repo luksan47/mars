520f700d150c0be99fad39221ac24b39567b39b7
Norbert Luksa <norbert.luksa@gmail.com>
2020-06-23 19:55:04 +0000

    Apply fixes from StyleCI

--- resources/lang/en/general.php
+++ resources/lang/en/general.php
@@ -23,6 +23,6 @@ return [
     'successful_modification' => 'Successful modification!',
     'successful_transaction' => 'Successful transaction!',
     'successfully_added' => 'Successfully added!',
-    'choose_option' => 'Choose an option...'
+    'choose_option' => 'Choose an option...',
 
 ];
