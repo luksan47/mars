37d9b5417d4b80bd536f2f56161618afbbd2b7b2
kdominik <56202545+katkodominik@users.noreply.github.com>
2020-08-15 15:54:24 +0200

    Merged Localization- and LocaleController

--- app/Http/Controllers/LocaleController.php
+++ app/Http/Controllers/LocaleController.php
@@ -4,6 +4,9 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\App;
+use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Auth;
+use App\LocalizationContribution;
 
 class LocaleController extends Controller
 {
@@ -28,5 +31,58 @@ class LocaleController extends Controller
             }
         }
         return view('locale.app')->with('locale', $locale);
+    }
+
+    /**
+     * Localization Contributions 
+     */ 
+
+    public function index()
+    {
+        return view('localizations', ['contributions'=> LocalizationContribution::all()]);
+    }
+
+    public function indexAdmin()
+    {
+        return view('admin.localizations', ['contributions'=> LocalizationContribution::where('approved', false)->get()]);
+    }
+
+    public function add(Request $request)
+    {
+        $user = Auth::user();
+        LocalizationContribution::create([
+            'language' => $request->language,
+            'key' => $request->key,
+            'value' => $request->value,
+            'contributor_id' => $user->id,
+        ]);
+
+        return back()->with('message', __('general.successful_modification'));
+    }
+
+    public function approve(Request $request)
+    {
+        $contribution = LocalizationContribution::findOrFail($request->id);
+        $exitCode = Artisan::call('locale:add', [
+            'language' => $contribution->language,
+            'key' => $contribution->key,
+            'value' => $contribution->value,
+            '--force' => 'true',
+        ]);
+        if ($exitCode == 0) {
+            $contribution->update(['approved' => true]);
+
+            return back()->with('message', __('general.successful_modification'));
+        }
+
+        return back()->with('message', 'Something went wrong. Artisan error code: '.$exitCode);
+    }
+
+    public function delete(Request $request)
+    {
+        $contribution = LocalizationContribution::findOrFail($request->id);
+        $contribution->delete();
+
+        return back()->with('message', __('general.successful_modification'));
     }
 }
