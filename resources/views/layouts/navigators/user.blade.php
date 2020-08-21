a1abc472a51be14426d215009bb5768c263f923e
katkodominik <56202545+katkodominik@users.noreply.github.com>
2020-08-22 00:47:08 +0200

    Sidenav UI improvements

--- resources/views/layouts/navigators/user.blade.php
+++ resources/views/layouts/navigators/user.blade.php
@@ -1,6 +1,6 @@
 @guest
-    <li><a href="{{ route('login') }}">@lang('general.login')</a></li>
-    <li><a href="{{ route('register') }}">@lang('general.register')</a></li>
+    <li><a class="waves-effect" href="{{ route('login') }}">@lang('general.login')</a></li>
+    <li><a class="waves-effect" href="{{ route('register') }}">@lang('general.register')</a></li>
 @else
-    <li><a href="{{ route('user') }}"><i class="material-icons left">account_circle</i>{{ Auth::user()->name }}</a></li>
+    <li><a class="waves-effect" href="{{ route('user') }}"><i class="material-icons left">account_circle</i>{{ Auth::user()->name }}</a></li>
 @endguest
