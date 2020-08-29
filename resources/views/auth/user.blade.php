83f288edaffc706f44857e87863390b90d690f3f
Katkó Dominik <56202545+kdmnk@users.noreply.github.com>
2020-08-29 12:05:07 +0200

    Added more info to user page (#290)
    Co-authored-by: katkodominik <56202545+katkodominik@users.noreply.github.com>

--- resources/views/auth/user.blade.php
+++ resources/views/auth/user.blade.php
@@ -83,32 +83,68 @@
                             </td>
                         </tr>
                         @endif
-                        {{-- Neptun code, faculties and workshops --}}
+                        {{-- Other personal information --}}
+                        @if($user->hasPersonalInformation())
+                            <tr>
+                                <th scope="row">@lang('info.place_and_date_of_birth')</th>
+                                <td>
+                                    {{ $user->personalInformation->place_of_birth }},  {{ $user->personalInformation->date_of_birth }}
+                                </td>
+                            </tr>
+                            <tr>
+                                <th scope="row">@lang('info.mothers_name')</th>
+                                <td>
+                                    {{ $user->personalInformation->mothers_name }}
+                                </td>
+                            </tr>
+                            <tr>
+                                <th scope="row">@lang('info.address')</th>
+                                <td>
+                                    {{ $user->personalInformation->country }}, <small>{{ $user->personalInformation->county }}</small>
+                                    <br>
+                                    {{ $user->personalInformation->zip_code }} {{ $user->personalInformation->city }},
+                                    <small>{{ $user->personalInformation->street_and_number }} </small>
+                                </td>
+                            </tr>
+                        @endif
+                        {{-- Other educational information --}}
                         @if($user->hasEducationalInformation())
-                        <tr>
-                            <th scope="row">@lang('info.neptun')</th>
-                            <td>{{ $user->educationalInformation->neptun ?? ''}}</td>
-                        </tr>
-                        <tr>
-                            <th scope="row">@lang('info.faculty')</th>
-                            <td>
-                                <ul>
-                                    @foreach ($user->faculties as $faculty)
-                                    <li>{{$faculty->name}}</li>
-                                    @endforeach
-                                </ul>
-                            </td>
-                        </tr>
-                        <tr>
-                            <th scope="row">@lang('info.workshop')</th>
-                            <td>
-                                <ul>
-                                    @foreach ($user->workshops as $workshop)
-                                    <li>{{$workshop->name}}</li>
-                                    @endforeach
-                                </ul>
-                            </td>
-                        </tr>
+                            <tr>
+                                <th scope="row">@lang('info.high_school')</th>
+                                <td>{{ $user->educationalInformation->high_school }}</td>
+                            </tr>
+                            <tr>
+                                <th scope="row">@lang('info.year_of_graduation')</th>
+                                <td>{{ $user->educationalInformation->year_of_graduation }}</td>
+                            </tr>
+                            <tr>
+                                <th scope="row">@lang('info.year_of_acceptance')</th>
+                                <td>{{ $user->educationalInformation->year_of_acceptance }}</td>
+                            </tr>
+                            <tr>
+                                <th scope="row">@lang('info.neptun')</th>
+                                <td>{{ $user->educationalInformation->neptun ?? ''}}</td>
+                            </tr>
+                            <tr>
+                                <th scope="row">@lang('info.faculty')</th>
+                                <td>
+                                    <ul>
+                                        @foreach ($user->faculties as $faculty)
+                                        <li>{{$faculty->name}}</li>
+                                        @endforeach
+                                    </ul>
+                                </td>
+                            </tr>
+                            <tr>
+                                <th scope="row">@lang('info.workshop')</th>
+                                <td>
+                                    <ul>
+                                        @foreach ($user->workshops as $workshop)
+                                        <li>{{$workshop->name}}</li>
+                                        @endforeach
+                                    </ul>
+                                </td>
+                            </tr>
                         @endif
                     </tbody>
                 </table>
