ab7c67231432eb534382b1f0c7a6d5d0e3012e8e
katkodominik <56202545+katkodominik@users.noreply.github.com>
2020-08-29 20:18:36 +0200

    Update AddExpression.php

--- app/Console/Commands/AddExpression.php
+++ app/Console/Commands/AddExpression.php
@@ -72,7 +72,7 @@ class AddExpression extends Command
                 return 1;
             }
             if (! is_dir(base_path('/resources/lang/'.$language))) {
-                mkdir(base_path('/resources/lang/'.$language), 0777, true); // create folders if needed
+                mkdir(base_path('/resources/lang/'.$language), 0755, true); // create folders if needed
             }
             $file_write = fopen(base_path($path), 'w');
             if (! (generate_file($file_write, $expressions))) {
