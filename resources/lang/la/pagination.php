206505e503804de82e1e62d90b0b36b83f57c5fb
katkodominik <56202545+katkodominik@users.noreply.github.com>
2020-08-30 11:03:15 +0200

    deleted files, updated gitignore

--- resources/lang/la/pagination.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-return [
-    'next' => 'Posterus &raquo;',
-    'previous' => '&laquo; Prior',
-];
--- resources/lang/sp/passwords.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-    'reset' => '¡Tu contraseña ha sido reajustada!',
-    'resetpwd' => 'Reajustar contraseña',
-    'sent' => '¡Te hemos enviado un e-mail con el enlace de reajuste de contraseña!',
-    'token' => 'Esta clave de reajuste de contraseña es inválida .',
-    'user' => 'No encontramos a ningún usuario con este e-mail.',
-];
