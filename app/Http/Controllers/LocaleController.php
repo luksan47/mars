6e589c613412aaeba780440f2665a35484fa11ff
Norbert Luksa <norbert.luksa@gmail.com>
2020-08-31 07:29:45 +0200

    Test verified and unverified routes (#314)

--- app/Http/Controllers/LocaleController.php
+++ app/Http/Controllers/LocaleController.php
@@ -18,26 +18,9 @@ class LocaleController extends Controller
         return redirect()->back()->cookie('locale', $locale, config('app.locale_cookie_lifespan'));
     }
 
-    public function list()
-    {
-        $locale = [];
-        $languages = array_diff(scandir(base_path('resources/lang/')), ['..', '.']);
-        foreach ($languages as $language) {
-            $files = array_diff(scandir(base_path('resources/lang/'.$language)), ['..', '.']);
-            foreach ($files as $file_in) {
-                if ($file_in != 'validation.php') {
-                    $name = substr($file_in, 0, strlen($file_in) - 4);
-                    $expressions = require base_path('resources/lang/'.$language.'/'.$file_in);
-                    $locale[$language][$name] = $expressions;
-                }
-            }
-        }
-        return view('locale.app')->with('locale', $locale);
-    }
-
     /**
-     * Localization Contributions 
-     */ 
+     * Localization Contributions
+     */
 
     public function index()
     {
@@ -46,7 +29,7 @@ class LocaleController extends Controller
             ->where('approved', true)
             ->groupBy('contributor_id')
             ->get();
-        
+
         $contributors = [];
         foreach ($contributor_ids as $value) {
             $contributor = User::find($value->id);
