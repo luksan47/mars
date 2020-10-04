65122929904647d840ec98a77b918054563bf223
Norbert Luksa <norbert.luksa@cloudera.com>
2020-10-04 23:20:00 +0200

    fine tune checkout permissions

--- app/Policies/TransactionPolicy.php
+++ app/Policies/TransactionPolicy.php
@@ -4,6 +4,7 @@ namespace App\Policies;
 
 use App\Transaction;
 use App\User;
+use App\Role;
 use Illuminate\Auth\Access\HandlesAuthorization;
 
 class TransactionPolicy
@@ -12,6 +13,10 @@ class TransactionPolicy
 
     public function delete(User $user, Transaction $transaction)
     {
-        return $user->hasRole(Role::STUDENT_COUNCIL);
+        // No transaction that has been moved to checkout can be deleted.
+        if ($transaction->moved_to_checkout){
+            return false;
+        }
+        return $user->hasRoleWithObjectNames(Role::STUDENT_COUNCIL, ['economic-member', 'economic-leader']);
     }
 }
