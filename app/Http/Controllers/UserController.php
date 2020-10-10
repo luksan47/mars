34457a06bdda6721bd139b692d644463eabaa3e6
Norbert Luksa <luksan47@users.noreply.github.com>
2020-10-10 08:58:36 +0000

    Apply fixes from StyleCI

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,8 +2,8 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\User;
 use App\Models\Role;
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
