3a5786ff11c20d4303e36488c5b9a15aa78d4090
Norbert Luksa <norbert.luksa@cloudera.com>
2020-10-11 10:51:29 +0200

    Formatting for roles.blade

--- resources/views/user/roles.blade.php
+++ resources/views/user/roles.blade.php
@@ -1,17 +1,21 @@
-@foreach($roles->chunkWhile(function($current, $key, $chunk) {
-    return $current->name() === $chunk->last()->name();
-}) as $rolegroup)
-<span class="new badge {{ $rolegroup->first()->color() }}" data-badge-caption="">
-    <nobr>{{ $rolegroup->first()->name() }}</nobr>
-</span>
-    @foreach($rolegroup as $role)
-    @if($role->object())
+{{-- Input: $roles, $newline = false --}}
+{{-- Show roles in badges. Roles with same base will be grouped. --}}
+@php
+$chunks = $roles->chunkWhile(function($current, $key, $chunk) {
+    return $current->name === $chunk->last()->name;
+});
+@endphp
+
+@foreach($chunks as $rolegroup)
     <span class="new badge {{ $rolegroup->first()->color() }}" data-badge-caption="">
-    <nobr>: {{ $role->object()->name }}</nobr>
+        <nobr>{{ $rolegroup->first()->name() }}</nobr>
     </span>
-    @endif
+    @foreach($rolegroup as $role)
+        @if($role->object())
+        <span class="new badge {{ $rolegroup->first()->color() }}" data-badge-caption="">
+            <nobr>: {{ $role->object()->name }}</nobr>
+        </span>
+        @endif
     @endforeach
-@if($newline ?? false)
-<br>
-@endif
+    @if($newline ?? false) <br> @endif
 @endforeach
