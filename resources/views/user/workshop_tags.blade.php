0981140460c02687c3e03f74277696dca4b0f810
Katkó Dominik <katkodominik@gmail.com>
2020-10-18 21:16:48 +0200

    fix

--- resources/views/user/workshop_tags.blade.php
+++ resources/views/user/workshop_tags.blade.php
@@ -1,8 +1,8 @@
 @can('viewEducationalInformation', $user)
 @if($user->hasEducationalInformation())
 @foreach ($user->workshops as $workshop)
-    <span class="new badge {{ $workshop->color() }}" id="user-workshop-{{ $user->id }}-{{ $workshop->id }}" data-badge-caption="">
-        <nobr>{{$workshop->name}} <span style="cursor: pointer;" onclick="valami({{ $user->id }}, {{ $workshop->id }})">&cross;</span></nobr>
+    <span class="new badge {{ $workshop->color() }} scale-transition" id="user-workshop-{{ $user->id }}-{{ $workshop->id }}" data-badge-caption="">
+        <nobr>{{$workshop->name}} <span style="cursor: pointer;" onclick="deleteWorkshop({{ $user->id }}, {{ $workshop->id }})">&cross;</span></nobr>
     </span>
     @if($newline ?? false)
     <br id="br-user-workshop-{{ $user->id }}-{{ $workshop->id }}">
@@ -11,8 +11,7 @@
 
 {{-- TODO: ADD SCRIPT CONTENT --}}
 <script>
-    function valami(userId, workshopId) {
-        console.log(userId, workshopId);
+    function deleteWorkshop(userId, workshopId) {
         $route = "{{ route('secretariat.user.workshop.delete', ['user' => ':user', 'workshop' => ':workshop']) }}"
             .replace(':user', userId)
             .replace(':workshop', workshopId);
@@ -20,8 +19,9 @@
             type: "GET",
             url: $route,
             success: function () {
-                $('#user-workshop-' + userId + '-' + workshopId).remove();
-                $('#br-user-workshop-' + userId + '-' + workshopId).remove();
+                $('#user-workshop-' + userId + '-' + workshopId).addClass("scale-out");
+                //TODO remove entirely
+                M.toast({html: "@lang('general.successfully_deleted')"});
             },
             error: function(error) {
                 console.log("TODO");
