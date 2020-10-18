d529ac185a242b403e67211164d56208cd5b97fe
Katkó Dominik <katkodominik@gmail.com>
2020-10-18 20:23:54 +0200

    added delete buttons

--- resources/views/user/workshop_tags.blade.php
+++ resources/views/user/workshop_tags.blade.php
@@ -2,7 +2,7 @@
 @if($user->hasEducationalInformation())
 @foreach ($user->workshops as $workshop)
     <span class="new badge {{ $workshop->color() }}" data-badge-caption="">
-        <nobr>{{$workshop->name}}</nobr>
+        <nobr>{{$workshop->name}} <span style="cursor: pointer;">&cross;</span></nobr>
     </span>
     @if($newline ?? false)
     <br>
