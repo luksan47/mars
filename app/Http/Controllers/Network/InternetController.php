f546529c731e79c1b4d80de4d0d98e6e49aa0791
Norbert Luksa <luksan47@users.noreply.github.com>
2020-10-20 19:30:39 +0000

    Apply fixes from StyleCI

--- app/Http/Controllers/Network/InternetController.php
+++ app/Http/Controllers/Network/InternetController.php
@@ -12,7 +12,6 @@ use App\Models\WifiConnection;
 use App\Utils\TabulatorPaginator;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\Str;
 
