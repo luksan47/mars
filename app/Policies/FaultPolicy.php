c558da2e20823b5438750037e63070661f67fbdb
Barbara <szabo.barbara.noemi@gmail.com>
2020-11-23 19:05:33 +0100

    416 issue

---
+++ app/Policies/FaultPolicy.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Policies;
+
+use App\Models\Fault;
+use App\Models\User;
+use App\Models\Role;
+use Illuminate\Auth\Access\HandlesAuthorization;
+
+class FaultPolicy
+{
+    use HandlesAuthorization;
+
+    // /**
+    // * Determine whether the user can view any fault.
+    // *
+    // * @param  \App\Models\User  $user
+    // * @return mixed
+    // */
+    //public function viewAny(User $user)
+    //{
+    //    return true;
+    //}
+
+    /**
+     * Determine whether the user can create fault.
+     *
+     * @param  \App\Models\User  $user
+     * @return mixed
+     */
+    public function create(User $user)
+    {
+        return $user->hasRole(Role::STAFF) || $user->hasRole(Role::COLLEGIST) || $user->hasRole(Role::TENANT);
+    }
+
+    /**
+     * Determine whether the user can update the status of the fault.
+     *
+     * @param  \App\Models\User  $user
+     * @param  \App\Models\Fault  $fault
+     * @return mixed
+     */
+    public function updateFaultStatus(User $user, Fault $fault)
+    {
+        return $user->hasRole(Role::STAFF) || $fault === Fault::UNSEEN;
+    }
+}
--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -23,6 +23,7 @@ class AuthServiceProvider extends ServiceProvider
         \App\Models\User::class => \App\Policies\UserPolicy::class,
         \App\Models\Checkout::class => \App\Policies\CheckoutPolicy::class,
         \App\Models\Transaction::class => \App\Policies\TransactionPolicy::class,
+        \App\Models\Fault::class => \App\Policies\FaultPolicy::class,
     ];
 
     /**
