9e382091a5f0f4896a7b0079dffe46245f767e47
katkodominik <56202545+katkodominik@users.noreply.github.com>
2020-12-27 00:10:15 +0100

    Mail basics

--- app/Http/Controllers/StudentsCouncil/EpistolaController.php
+++ app/Http/Controllers/StudentsCouncil/EpistolaController.php
@@ -5,8 +5,9 @@ namespace App\Http\Controllers\StudentsCouncil;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\Mail;
-use Illuminate\Support\Facades\Validator;
+use App\Notifications\EpistolaCollegii;
+use Image;
+
 use App\Models\EpistolaNews;
 
 class EpistolaController extends Controller
@@ -14,5 +15,12 @@ class EpistolaController extends Controller
     public function index(Request $request)
     {
         return view('student-council.communicational-committee.epistola', ['news' => EpistolaNews::all()->sortByDesc('valid_until')]);
+    }
+
+    public function send()
+    {
+        $this->authorize('send', EpistolaNews::class);
+        return (new EpistolaCollegii())->toMail();
+
     }
 }
--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -1,17 +1,8 @@
 <tr>
 <td class="header" style="vertical-align:middle">
 <a href="{{ $url }}" style="display: inline-block;">
-@switch (trim($slot))
-@case('Laravel')
-<img src="https://laravel.com/img/notification-logo.png" class="logo" alt="Laravel Logo">
-@break
-@case('Urán')
 <img src="{{ asset('img/uran_blue.png') }}" width="60" alt="Urán Logó">
 <span class="logo" > {{ config('app.name') }} </span>
-@break
-@default
-{{ $slot }}
-@endswitch
 </a>
 </td>
 </tr>
