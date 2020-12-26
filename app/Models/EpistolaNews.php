c14f28cd53e1b61923fe8783160199cc0a7cee63
katkodominik <56202545+katkodominik@users.noreply.github.com>
2020-12-26 02:01:53 +0100

    basics

---
+++ app/Models/EpistolaNews.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class EpistolaNews extends Model
+{
+    use HasFactory;
+
+    protected $table = 'epistola';
+
+    public $timestamps = false;
+
+    protected $fillable = [
+        'title',
+        'subtitle',
+        'description', //main text
+        'further_details',
+        'website',
+        'facebook_event',
+        'registration',
+        'registration_deadline',
+        'filling_deadline',
+        'date',
+        'end_date', //time interval if both provided
+        'picture', //path to main picture
+        'valid_until' //notifications should be sent before this date
+    ];
+}
