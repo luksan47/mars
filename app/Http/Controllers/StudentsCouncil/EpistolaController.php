d70dec9b41ab3d79247e8779418f9f59489d93cb
katkodominik <56202545+katkodominik@users.noreply.github.com>
2020-12-27 18:11:46 +0100

    send email

--- app/Http/Controllers/StudentsCouncil/EpistolaController.php
+++ app/Http/Controllers/StudentsCouncil/EpistolaController.php
@@ -6,7 +6,8 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\Facades\Auth;
-use App\Notifications\EpistolaCollegii;
+use Illuminate\Support\Facades\Mail;
+use App\Mail\EpistolaCollegii;
 use Image;
 
 use App\Models\EpistolaNews;
@@ -15,7 +16,7 @@ class EpistolaController extends Controller
 {
     public function index()
     {
-        return view('student-council.communication-committee.epistola', ['news' => EpistolaNews::all()->sortBy('valid_until')]);
+        return view('student-council.communication-committee.epistola', ['news' => EpistolaNews::where('sent', false)->get()->sortBy('valid_until')]);
     }
 
     public function edit(EpistolaNews $epistola)
@@ -79,10 +80,23 @@ class EpistolaController extends Controller
         return redirect(route('epistola'))->with('message', $updated ? __('general.successful_modification') : __('general.successfully_added'));
     }
 
+    public function preview()
+    {
+        $this->authorize('send', EpistolaNews::class);
+
+        return (new EpistolaCollegii(EpistolaNews::where('sent', false)->get()))->toMail();
+    }
+
     public function send()
     {
         $this->authorize('send', EpistolaNews::class);
-        return (new EpistolaCollegii())->toMail();
+
+        $mail = new EpistolaCollegii(EpistolaNews::where('sent', false)->get());
+        Mail::to(env('MAIL_KOMMBIZ'))->send($mail);
+
+        EpistolaNews::where('sent', false)->update(['sent' => true]);
+
+        return back()->with('message', 'Elküldve a Bizottság e-mail címére.');
 
     }
 }
---
+++ app/Mail/EpistolaCollegii.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+
+class EpistolaCollegii extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    /**
+     * Create a new message instance.
+     *
+     * @return void
+     */
+    public function __construct($epistolas)
+    {
+        $this->epistolas = $epistolas;
+        $this->theme = 'epistola';
+    }
+
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    public function build()
+    {
+        return $this
+            ->subject('Epistola Collegii - ' . now()->format('Y. m. d.'))
+            ->markdown('emails.epistola', ['news' => $this->epistolas]);
+    }
+}
