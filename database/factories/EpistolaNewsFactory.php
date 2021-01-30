03448b3bb013c1c716cb20fde03900ba03e40955
Norbert Luksa <luksan47@users.noreply.github.com>
2021-01-30 16:56:02 +0000

    Apply fixes from StyleCI

--- database/factories/EpistolaNewsFactory.php
+++ database/factories/EpistolaNewsFactory.php
@@ -25,7 +25,7 @@ class EpistolaNewsFactory extends Factory
             'uploader_id' => \App\Models\User::where('verified', true)->get()->random()->id,
             'title' => $this->faker->realText($maxNbChars = 50),
             'subtitle' => $this->faker->sentence(),
-            'description' => $this->faker->paragraphs($this->faker->numberBetween(1,3), true),
+            'description' => $this->faker->paragraphs($this->faker->numberBetween(1, 3), true),
             'further_details_url' => $this->faker->boolean(50) ? $this->faker->url() : null,
             'website_url' => $this->faker->boolean(50) ? $this->faker->url() : null,
             'facebook_event_url' => $this->faker->boolean(50) ? $this->faker->url() : null,
@@ -38,11 +38,14 @@ class EpistolaNewsFactory extends Factory
                 if ($attributes['date'] != null && $this->faker->boolean(50)) {
                     return now()->addMinutes($this->faker->numberBetween(0, 3600));
                 }
+
                 return null;
             },
             'end_date' => function (array $attributes) {
-                if ($attributes['date'] != null && $attributes['time'] == null && $this->faker->boolean(50))
+                if ($attributes['date'] != null && $attributes['time'] == null && $this->faker->boolean(50)) {
                     return now()->addDays($this->faker->numberBetween(0, 200));
+                }
+
                 return null;
             },
             'picture_path' => $this->faker->boolean(50) ? $this->faker->imageUrl(640, 480, 'animals', true) : null,
