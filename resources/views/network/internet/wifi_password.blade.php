6efd02acdc1fb95f871b74a7c4b8e1c10a4bce50
Viktor Csimma <csimmaviktor03@gmail.com>
2021-10-02 23:13:17 +0200

    Changed font family, added a copy button

--- resources/views/network/internet/wifi_password.blade.php
+++ resources/views/network/internet/wifi_password.blade.php
@@ -8,13 +8,13 @@
             <tbody>
                 <tr>
                     <td>@lang('internet.wifi_user')</td>
-                    <td class="right">{{ $internet_access->wifi_username }}</td>
+                    <td class="right" style="font-family: Monospace;">{{ $internet_access->wifi_username }}</td>
                 </tr>
                 <tr>
                     @if($internet_access->wifi_password != null)
                     <td>@lang('internet.wifi_password')</td>
-                    <td class="right"><span onclick="$(this).text('{{ $internet_access->wifi_password }}');"
-                            style="cursor: pointer;">
+                    <td class="right"><span id="pw" onclick="$(this).text('{{ $internet_access->wifi_password }}');"
+                            style="cursor: pointer; font-family: Monospace;">
                             <button class="waves-effect btn-small">@lang('internet.show')</button></span>
                     </td>
                     @else
@@ -29,6 +29,9 @@
                     <p>@lang('internet.wifi_password_reset_warning')</p>
                 </blockquote>
             </div>
+            <div id="copydiv" style="display: none;" class="col s12 m12 l12 xl8">
+                <button id="cp_btn" class="btn right" value="copy" onclick="copyToClipboard()">@lang('internet.copy')</button>
+            </div>
             <div class="input-field col s12 m12 l12 xl4">
                 <form action="{{ route('internet.wifi_password.reset') }}" method="post">
                     @csrf
@@ -38,3 +41,23 @@
         </div>
     </div>
 </div>
+
+<script>
+document.getElementById("pw").addEventListener("click", show_button);
+
+function show_button() {
+    document.getElementById("copydiv").style.display='block';
+}
+
+document.getElementById("cp_btn").addEventListener("click", copy_password);
+
+function copy_password() {
+    var copyText = document.getElementById("pw");
+    var textArea = document.createElement("textarea");
+    textArea.value = copyText.textContent;
+    document.body.appendChild(textArea);
+    textArea.select();
+    document.execCommand("Copy");
+    textArea.remove();
+}
+</script>
