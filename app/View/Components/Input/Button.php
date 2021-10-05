0ef2b44e71b6e1ce13122ead63dca410691a02e9
Norbert Luksa <luksan47@users.noreply.github.com>
2021-10-05 20:59:36 +0000

    Apply fixes from StyleCI

--- app/View/Components/Input/Button.php
+++ app/View/Components/Input/Button.php
@@ -11,6 +11,7 @@ class Button extends Input
 
     /**
      * Create a new button instance. The button will be a link if the href attribute is provided.
+     *
      * @param $floating if provided, the button will be a floating action button. The icon attribute is recommended.
      * @param $icon if provided, an icon will be placed in the button instead of a text
      */
