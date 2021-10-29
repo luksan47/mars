d5c4bd8af08472f10ba915d9a9f129d52758b13f
viktorcsimma <70488334+viktorcsimma@users.noreply.github.com>
2021-10-29 11:30:28 +0200

    #452: Copy workshops to role.php language file (#626)
    * Moved workshop names to role.php, add_workshop dropdown is ugly
    
    * Simplified translation in the dropdown
    
    * Removed unused reference from Workshop.php
    
    * Switched back to x-input and old names
    
    * Removed unused reference
    
    Co-authored-by: Katkó Dominik <56202545+kdmnk@users.noreply.github.com>

--- resources/views/user/workshop_tags.blade.php
+++ resources/views/user/workshop_tags.blade.php
@@ -2,7 +2,7 @@
     @if($user->hasEducationalInformation())
         @foreach ($user->workshops as $workshop)
             <span class="new badge {{ $workshop->color() }} scale-transition" id="user-workshop-{{ $user->id }}-{{ $workshop->id }}" data-badge-caption="">
-                <nobr>{{$workshop->name}} 
+                <nobr>@lang('role.'.$workshop->name)
                     @can('viewAny', \App\Models\User::class) <!-- delete button -->
                         <span style="cursor: pointer;" onclick="deleteWorkshop({{ $user->id }}, {{ $workshop->id }})">&cross;</span>
                     @endcan
