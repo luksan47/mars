52e291a561bcc75752072ffaf42dc58d2eb9132d
Katkó Dominik <56202545+kdmnk@users.noreply.github.com>
2021-10-30 13:50:29 +0200

    Add constraints to role assignment (#630)
    * make unique functions and user getters
    
    * Apply fixes from StyleCI
    
    * made the functions static
    
    * response messages
    
    * fix
    
    * Apply fixes from StyleCI
    
    * remove logs
    
    * more comments
    
    * more comments
    
    * Apply fixes from StyleCI
    
    * Made it nicer
    
    * Apply fixes from StyleCI
    
    * fix rebase
    
    * Changes based on comments
    
    * Apply fixes from StyleCI
    
    * test
    
    * Apply fixes from StyleCI
    
    Co-authored-by: Norbert Luksa <luksan47@users.noreply.github.com>
    Co-authored-by: vagrant <vagrant@homestead>
    Co-authored-by: katkodominik <56202545+katkodominik@users.noreply.github.com>
    Co-authored-by: Norbert Luksa <norbert.luksa@gmail.com>

--- app/Models/Role.php
+++ app/Models/Role.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Facades\Cache;
 
@@ -20,6 +21,42 @@ class Role extends Model
     const LOCALE_ADMIN = 'locale-admin';
     const PERMISSION_HANDLER = 'permission-handler';
     const STUDENT_COUNCIL = 'student-council';
+
+    //Students' Committe role's objects
+    const PRESIDENT = 'president';
+    const VICE_PRESIDENT = 'vice-president';
+    const ECONOMIC_LEADER = 'economic-leader';
+    const ECONOMIC_MEMBER = 'economic-member';
+    const CULTURAL_LEADER = 'cultural-leader';
+    const CULTURAL_MEMBER = 'cultural-member';
+    const COMMUNITY_LEADER = 'community-leader';
+    const COMMUNITY_MEMBER = 'community-member';
+    const COMMUNICATION_LEADER = 'communication-leader';
+    const COMMUNICATION_MEMBER = 'communication-member';
+    const SPORT_LEADER = 'sport-leader';
+    const SPORT_MEMBER = 'sport-member';
+    const SCIENCE_LEADER = 'science-leader';
+    const SCIENCE_MEMBER = 'science-member';
+    const STUDENT_COUNCIL_LEADERS = [
+        self::PRESIDENT,
+        self::VICE_PRESIDENT
+    ];
+    const COMMITTEE_LEADERS = [
+        self::ECONOMIC_LEADER,
+        self::CULTURAL_LEADER,
+        self::COMMUNITY_LEADER,
+        self::COMMUNICATION_LEADER,
+        self::SPORT_LEADER,
+        self::SCIENCE_LEADER
+    ];
+    const COMMITTEE_MEMBERS = [
+        self::ECONOMIC_MEMBER,
+        self::CULTURAL_MEMBER,
+        self::COMMUNITY_MEMBER,
+        self::COMMUNICATION_MEMBER,
+        self::SPORT_MEMBER,
+        self::SCIENCE_MEMBER
+    ];
 
     // Module-related roles
     const PRINTER = 'printer';
@@ -61,6 +98,52 @@ class Role extends Model
     }
 
     /**
+     * Returns true if the role can be attached to only one user at a time.
+     * @param int $roleId
+     * @param string $objectName optional. Returns false if the object is null for a role which can have objects.
+     * @return bool
+     */
+    public static function isUnique($roleName, $objectName = null): bool
+    {
+        switch ($roleName) {
+            case self::DIRECTOR:
+                return true;
+            case self::WORKSHOP_ADMINISTRATOR:
+                return true;
+            case self::WORKSHOP_LEADER:
+                return true;
+            case self::STUDENT_COUNCIL:
+                return in_array($objectName, array_merge(self::STUDENT_COUNCIL_LEADERS, self::COMMITTEE_LEADERS, self::COMMITTEE_MEMBERS));
+            default:
+                return false;
+        }
+    }
+
+    /**
+     * Checks if the specified role can be attached to someone.
+     * Object id is required if the role can have objects.
+     * @param int $roleId
+     * @param int $objectId optional
+     * @return bool.
+     */
+    public static function canBeAttached($roleId, $objectId = null): bool
+    {
+        $role = self::findOrFail($roleId);
+        if($role->canHaveObject())
+        {
+            if($objectId == null) return false;
+            $object = $role->possibleObjects()->firstWhere('id', $objectId)->name;
+        }
+
+        if (self::isUnique($role->name, ($object ?? null))) {
+            return User::whereHas('roles', function (Builder $query)  use ($roleId, $objectId){
+                $query->where('id', $roleId)->where('role_users.object_id', $objectId);
+            })->count() < 1;
+        }
+        return true;
+    }
+
+    /**
      * Returns the object (with id and name) of the role or null if the role does not have.
      */
     public function object()
@@ -72,15 +155,20 @@ class Role extends Model
         return $this->possibleObjects()->where('id', $this->pivot->object_id)->first();
     }
 
-    public static function getObjectIdByName($role, $objectName)
+    public static function getObjectIdByName($roleName, $objectName)
     {
-        $objects = self::possibleObjectsFor($role);
-
+        $objects = self::possibleObjectsFor($roleName);
         return $objects->where('name', $objectName)->first()->id;
     }
 
-    public static function getUsers(string $roleName)
+    public static function getUsers(string $roleName, string $objectName = null)
     {
+        if(isset($objectName)){
+            return User::whereHas('roles', function ($q) use ($roleName, $objectName) {
+                $q->where('role_id', Role::getId($roleName))
+                    ->where('object_id', Role::getObjectIdByName($roleName, $objectName));
+            });
+        }
         return self::firstWhere('name', $roleName)->users;
     }
 
@@ -98,7 +186,7 @@ class Role extends Model
     }
 
     /**
-     * Returns if the role can have objects
+     * Returns if the role can have objects.
      * @return string object name
      */
     public static function canHaveObjectFor($name): bool
@@ -141,22 +229,7 @@ class Role extends Model
         }
 
         if ($name == self::STUDENT_COUNCIL) {
-            $student_council_members = [
-                'president',
-                'vice_president',
-                'economic-leader',
-                'communication-leader',
-                'community-leader',
-                'cultural-leader',
-                'sport-leader',
-                'science-leader',
-                'economic-member',
-                'communication-member',
-                'community-member',
-                'cultural-member',
-                'sport-member',
-                'science-member',
-            ];
+            $student_council_members = array_merge(self::STUDENT_COUNCIL_LEADERS, self::COMMITTEE_LEADERS, self::COMMITTEE_MEMBERS);
 
             return self::toSelectableCollection($student_council_members);
         }
@@ -170,7 +243,39 @@ class Role extends Model
             return self::toSelectableCollection($collegists);
         }
 
-        return [];
+        return collect([]);
+    }
+
+    public function hasElevatedPermissions(): bool
+    {
+        return in_array($this->name, [self::PRINT_ADMIN, self::NETWORK_ADMIN, self::PERMISSION_HANDLER, self::SECRETARY, self::DIRECTOR]);
+    }
+
+    public function hasTranslatedName(): bool
+    {
+        return in_array($this->name, [self::WORKSHOP_ADMINISTRATOR, self::WORKSHOP_LEADER]);
+    }
+
+    /**
+     * Create a collection with id and name of the items in an array.
+     * The ids starts at 1.
+     * @param array $items the items in the array will be the name attributes
+     * @return collection
+     */
+    private static function toSelectableCollection(array $items)
+    {
+        $objects = [];
+        $id = 1;
+        foreach ($items as $name) {
+            $objects[] = (object) ['id' => $id++, 'name' => $name];
+        }
+
+        return collect($objects);
+    }
+
+    public function isSysAdmin()
+    {
+        return in_array($this->name, [self::NETWORK_ADMIN]);
     }
 
     public function color()
@@ -207,37 +312,5 @@ class Role extends Model
             default:
                 return 'grey';
         }
-    }
-
-    public function hasElevatedPermissions(): bool
-    {
-        return in_array($this->name, [self::PRINT_ADMIN, self::NETWORK_ADMIN, self::PERMISSION_HANDLER, self::SECRETARY, self::DIRECTOR]);
-    }
-
-    public function hasTranslatedName(): bool
-    {
-        return in_array($this->name, [self::WORKSHOP_ADMINISTRATOR, self::WORKSHOP_LEADER]);
-    }
-
-    /**
-     * Create a collection with id and name of the items in an array.
-     * The ids starts at 1.
-     * @param array $items the items in the array will be the name attributes
-     * @return collection
-     */
-    private static function toSelectableCollection(array $items)
-    {
-        $objects = [];
-        $id = 1;
-        foreach ($items as $name) {
-            $objects[] = (object) ['id' => $id++, 'name' => $name];
-        }
-
-        return collect($objects);
-    }
-
-    public function isSysAdmin()
-    {
-        return in_array($this->name, [self::NETWORK_ADMIN]);
     }
 }
