59ad2ab7d2925cefc307db2d00a9b30e0dce76d1
Gábor <gabor.ungvri@gmail.com>
2022-01-19 15:42:36 +0100

    Correct code style

--- app/Policies/RouterPolicy.php
+++ app/Policies/RouterPolicy.php
@@ -3,8 +3,8 @@
 namespace App\Policies;
 
 use App\Models\Role;
-use App\Models\User;
 use App\Models\Router;
+use App\Models\User;
 use Illuminate\Auth\Access\HandlesAuthorization;
 
 class RouterPolicy
