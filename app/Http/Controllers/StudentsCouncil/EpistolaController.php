97e504a92e97ea9fa86734275f8c81644fa8b7b4
Katkó Dominik <56202545+kdmnk@users.noreply.github.com>
2022-03-03 11:00:03 +0100

    epistola and economic fix (#664)
    Co-authored-by: root <root@smaug.eotvos.elte.hu>

--- app/Http/Controllers/StudentsCouncil/EpistolaController.php
+++ app/Http/Controllers/StudentsCouncil/EpistolaController.php
@@ -23,14 +23,14 @@ class EpistolaController extends Controller
         $this->authorize('view', EpistolaNews::class);
 
         //sort by valid_until property with null values at the end
-        $unsent = EpistolaNews::where('sent', false)->get()->sortBy(function ($result) {
-            if ($result->valid_until == null)
-                if($result->date_for_sorting == null){
-                    return PHP_INT_MAX;
-                }
-                return $result->date_for_sorting;
-            return $result->valid_until;
-        });
+        $unsent = EpistolaNews::where('sent', false)->get();//->sortBy(function ($result) {
+            //if ($result->valid_until == null)
+            //    if($result->date_for_sorting == null){
+            //        return PHP_INT_MAX;
+            //    }
+            //    return $result->date_for_sorting;
+            //return $result->valid_until;
+        //});
 
         $sent = EpistolaNews::where('sent', true)->get();
         return view(
